0e39c9879b0517ca30c823c2fec41a0610e2401c
Kolapo Adedipe <kolapoadedipe36@gmail.com>
2023-04-12 05:12:25 -0700

    Task0-101

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""number_of_lines
+"""
+
+def write_file(filename="", text=""):
+    """Takes in str filename to read the number of lines
+    """
+
+    with open(filename, encoding="utf-8") as rea:
+        line = rea.write(text)
+        return len(line)
